c29c920dc8195d70dae28549c47ef2fdb9f31a61
Sajjad Mohammadi <radhoosh.user2@gmail.com>
2018-10-20 12:12:25 +0330

    added Card

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,10 +3,15 @@ import Button from './Button';
 import Code from './Code';
 import Label from './Label';
 import Input from './Input';
+import { Card, CardHeader, CardBody, CardFooter } from './Card';
 export {
     Alert,
     Button,
     Code,
     Label,
     Input,
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
 };
