2370985402027b3039ac6c16f87ab262da1de354
Sajjad Mohammadi <radhoosh.user2@gmail.com>
2018-10-20 11:13:11 +0330

    added Alert

---
+++ src/components/index.tsx
@@ -0,0 +1,19 @@
+import Alert from './Alert';
+import Button from './Button';
+import Label from './Label';
+import Input from './Input';
+import Code from './Code';
+import { Card, CardHeader, CardBody, CardFooter } from './Card';
+import { Navbar } from './Navbar';
+export {
+    Alert,
+    Button,
+    Label,
+    Input,
+    Code,
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Navbar
+};
